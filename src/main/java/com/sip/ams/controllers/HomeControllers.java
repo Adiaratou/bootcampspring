d0b81eeffe6f91378c64ec6db7744c24d7c3241d
AdiaratouB <oumar.berthe@sesame.com.tn>
2022-02-06 15:30:39 +0100

    passage data vers vue

--- src/main/java/com/sip/ams/controllers/HomeControllers.java
+++ src/main/java/com/sip/ams/controllers/HomeControllers.java
@@ -1,15 +1,19 @@
 package com.sip.ams.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeControllers {
 
 	@RequestMapping("/info")// ce que je tape dans l'url
-	public String info() {
-		
+	
+	public String info(Model model) {
+		String formation="Fullstack";
 		System.out.println("Méthode info");
+		model.addAttribute("workshop",formation);
+		
 		return "Home/info";// nom de la vue sous le template (chemin)
 	}
 	
